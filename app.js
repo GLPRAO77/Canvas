b6c1bd16f7616e684fc16b93938d970062c4d150
Anudeep <93270546+GLPRAO77@users.noreply.github.com>
2023-11-14 13:28:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -56,12 +56,12 @@ app.post('/canvas-demo/', function (req, res) {
         var json = decode(signed_request, process.env.CANVAS_CONSUMER_SECRET);
 
         //res.send(data);
-        res.render('index', { context: json, url: process.env.IMAGE_URL });
-        /*if (res.data.context.parameters.objectName == 'Home') {
+        //res.render('index', { context: json, url: process.env.IMAGE_URL });
+        if (res.data.context.environment.parameters.objectName == 'Home') {
             res.render('index', { context: json, url: process.env.IMAGE_URL });
-        } else if (res.data.context.parameters.objectName == 'Account') {
+        } else if (res.data.context.environment.parameters.objectName == 'Account') {
             res.render('account', { context: json, url: process.env.IMAGE_URL });
-        }*/
+        }
         //Render and pass
         //res.render('index', { context: json, url: process.env.IMAGE_URL });
     } else {
