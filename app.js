2cba57fc16d92ecaa50424e6ae160768f43eb4fb
Anudeep <93270546+GLPRAO77@users.noreply.github.com>
2023-11-14 12:23:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.post('/canvas-demo/', function (req, res) {
         body = JSON.stringify(req.body);
         data = JSON.stringify(res.data);
         console.log('data--' + data);
-        alert('1');
 
         signed_request = JSON.parse(body)['signed_request'];
 
